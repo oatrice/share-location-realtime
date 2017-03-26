27092273a9206a73922a9c0fad59f17b88fedfa5
oatrice msi <anirut.311@gmail.com>
2017-03-27 02:55:22 +0700

    dev

--- share-location-realtime-library/src/main/java/com/oatrice/ShareLocationRealtime/ShareLocationSigninActivity.kt
+++ share-location-realtime-library/src/main/java/com/oatrice/ShareLocationRealtime/ShareLocationSigninActivity.kt
@@ -2,7 +2,7 @@ package com.oatrice.ShareLocationRealtime
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-
+//
 class ShareLocationSigninActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
