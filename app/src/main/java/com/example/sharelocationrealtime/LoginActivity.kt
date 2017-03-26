83a2d4e0860d68e1167d62791d56952bcfb93b86
oatrice msi <anirut.311@gmail.com>
2017-03-27 02:43:10 +0700

    intent to lib activity

--- app/src/main/java/com/example/sharelocationrealtime/LoginActivity.kt
+++ app/src/main/java/com/example/sharelocationrealtime/LoginActivity.kt
@@ -1,13 +1,16 @@
 package com.example.sharelocationrealtime
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import junit.framework.Test
+import com.oatrice.ShareLocationRealtime.ShareLocationSigninActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val intent = Intent(this, ShareLocationSigninActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
