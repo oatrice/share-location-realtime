5a8be9a7857fb1d63b922a5c0761b0154025bd86
oatrice msi <anirut.311@gmail.com>
2017-03-27 02:54:55 +0700

    change activity name

--- app/src/main/java/com/example/sharelocationrealtime/MainActivity.kt
+++ app/src/main/java/com/example/sharelocationrealtime/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.oatrice.ShareLocationRealtime.ShareLocationSigninActivity
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
